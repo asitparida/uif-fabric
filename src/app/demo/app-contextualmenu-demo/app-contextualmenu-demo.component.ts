3609322758fec01f8dc870060526809e6f7a4d21
Asit Parida <asitparida@live.in>
2018-01-20 23:55:34 +0530

    Add uif-contextualmenu

---
+++ src/app/demo/app-contextualmenu-demo/app-contextualmenu-demo.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { UifContextualMenuDirectionHint } from '../../uif-fabric/uif-contextual-menu/uif-contextual-menu.models';
+
+@Component({
+	selector: 'app-contextualmenu-demo',
+	templateUrl: './app-contextualmenu-demo.component.html',
+})
+export class ContextualMenuDemoComponent {
+	openMenu1 = false;
+	openMenu2 = false;
+	openMenu3 = false;
+	dirHintMenu2  = UifContextualMenuDirectionHint.Top;
+	DirectionHints = UifContextualMenuDirectionHint;
+	menuItems = [
+		{ key: 'panda', text: 'Panda', selected: true },
+		{ key: 'koala', text: 'Koala', selected: false },
+		{ key: 'sloth', text: 'Sloth', selected: false }
+	];
+	menuItems2 = [
+		{ key: 'Ginger', text: 'Ginger', selected: true },
+		{ key: 'Pepper', text: 'Pepper', selected: false },
+		{ key: 'Cardamom', text: 'Cardamom', selected: false }
+	];
+	menuItems3 = [
+		{ key: 'panda', text: 'Panda', selected: true },
+		{ key: 'koala', text: 'Koala', selected: false },
+		{ key: 'sloth', text: 'Sloth', selected: false },
+		{ key: 'bear', text: 'Bear', selected: false },
+		{ key: 'boar', text: 'Boar', selected: false}
+	];
+	isMultiSelect = true;
+}
---
+++ src/app/uif-fabric/uif-contextual-menu/uif-contextual-menu.models.ts
@@ -0,0 +1,6 @@
+export enum UifContextualMenuDirectionHint {
+	Top = 1,
+	// Right = 2,
+	Bottom = 3
+	// Left = 4
+}
