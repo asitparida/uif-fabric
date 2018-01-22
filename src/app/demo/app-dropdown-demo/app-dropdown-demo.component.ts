a58b7b931b0f632294c9467d7fe053a7378eed75
Asit Parida <asitparida@live.in>
2018-01-22 22:14:01 +0530

    Add uif-dropdown basic ctrls  + css fixes

--- src/app/demo/app-dropdown-demo/app-dropdown-demo.component.ts
+++ src/app/demo/app-dropdown-demo/app-dropdown-demo.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
 	templateUrl: './app-dropdown-demo.component.html'
 })
 export class DropdownDemoComponent {
+	isMultiSelectable = true;
+	options = [
+		{ key: 'dogBarking', text: 'Dog Barking', selected: false },
+		{ key: 'mindBlowing', text: 'Wind Blowing', selected: true },
+		{ key: 'duckQuacking', text: 'Duck Quacking', selected: false },
+		{ key: 'cowMooing', text: 'Cow Mooing', selected: false }
+	];
+	isMultiSelectableHints = [
+		{ key: true, text: 'Yes'},
+		{ key: false, text: 'No'}
+	];
+	getJSON(val: any) {
+		return JSON.stringify(val);
+	}
+	getBoolean(val): boolean {
+		if (typeof val === 'boolean') {
+			return val;
+		} else {
+			return val === 'false' ? false : true;
+		}
+	}
 }
