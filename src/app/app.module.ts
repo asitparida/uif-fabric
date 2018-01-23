c707d16f48d421b2f9f88d57deadf0ae80493da2
Asit Parida <paridaa@vmware.com>
2018-01-23 22:49:13 +0530

    Adding auto definition generator for demo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { DemoModule } from './demo/demo.module';
 
 import { AppComponent } from './app.component';
+import { DemoerComponent, DemoerService } from './demoer/demoer.component';
 
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		DemoerComponent
 	],
 	imports: [
 		BrowserModule,
+		HttpModule,
 		DemoModule
 	],
-	providers: [],
+	providers: [
+		DemoerService
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/demoer/demoer.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit, Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DemoerService {
+	constructor(private http: Http) {}
+	getTsFile(id) {
+		const file = 'assets/definitions/' +  id  + '.component.ts.json';
+		return this.http.get(file).map((res: any) => res.json());
+	}
+	getMarkupFile(id) {
+		const file = 'assets/definitions/' +  id  + '.component.html.json';
+		return this.http.get(file).map((res: any) => res.json());
+	}
+}
+
+@Component({
+	selector: 'app-demoer',
+	templateUrl: './demoer.component.html',
+	styleUrls: ['../app.component.scss']
+})
+export class DemoerComponent implements OnInit {
+	@Input() id: string;
+	@Input() title: string;
+	tsTokenizedInfo;
+	markupTokenizedInfo;
+	showCodes = false;
+	showingTs = true;
+	constructor(private demoerService: DemoerService) {}
+	ngOnInit() {
+		this.demoerService.getTsFile(this.id).subscribe((jsonData) => {
+			this.tsTokenizedInfo = jsonData;
+		});
+		this.demoerService.getMarkupFile(this.id).subscribe((jsonData) => {
+			this.markupTokenizedInfo = jsonData;
+		});
+	}
+}
