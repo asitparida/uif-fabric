18e4e8ab6d3e16a2a19f7cc797c8daeb32737ab4
Asit Parida <paridaa@vmware.com>
2018-02-15 03:28:23 +0530

    Refactoring demo layout

---
+++ src/app/app-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-summary',
+	templateUrl: './app-summary.component.html',
+	styleUrls: ['./app.component.scss']
+})
+export class AppSummaryComponent {
+	constructor() {
+	}
+}
